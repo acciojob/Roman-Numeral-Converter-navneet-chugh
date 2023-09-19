16f4bb325292358fd980bd60dcf6b4558b8227d8
navneet-chugh <navneetchugh15@gmail.com>
2023-09-19 20:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,27 @@ function convertToRoman(num) {
     };
 
   //your code here
+	  let result = '';
 
+  for (let i = 0; i < 7; i += 2) {
+    const value = obj[i][1];
+    const letter = obj[i][0];
+    while (num >= value) {
+      result += letter;
+      num -= value;
+    }
+
+    if (i < 6) {
+      const nextValue = obj[i + 2][1];
+      const nextLetter = obj[i + 2][0];
+      if (num >= value - nextValue) {
+        result += nextLetter + letter;
+        num -= value - nextValue;
+      }
+    }
+  }
+
+  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
